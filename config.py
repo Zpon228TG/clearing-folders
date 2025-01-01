bdd1a1fade0832a847fd370e9e5753d7f070c45f
Zpon228TG <daetetet11@mail.ru>
2025-01-01 17:56:00 +0300

    Update and rename config (1).py to config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-api_id = 
-api_hash = ''
-bot_token = ''
-my_id = 
+api_id = 21619878
+api_hash = '7229cba001a73991540500b0ba92f463'
+bot_token = '7575098316:AAG-OiCSxagClYnVzR-4--yNkDYq_2a5mNo'
+my_id = 6578018656
 
-technical_channel_id =   # ID технического канала
+technical_channel_id = -1002397438868  # ID технического канала
 
 new_link = "http://t.me/"
 new_username = "@"
